c33c3a52c10777254e9172bb46a37ed3961fcfbf
KsenyaGorbovtsova <ksenchik97@bk.ru>
2017-10-27 16:58:58 +0300

    fc

--- lab4_1/librip/iterators.py
+++ lab4_1/librip/iterators.py
@@ -1,40 +1,32 @@
 class Unique(object):
-
     def __init__(self, items, **kwargs):
-
+        
         assert len(items) > 0
-
-        ignor_case = False
-        for key in kwargs:
-            ignor_case = kwargs[key]
-
-        self.lst = list(map(str, items))
-
-        if ignor_case is False:
-            self.lst.sort(key=lambda x: x.lower())
-        else:
-            self.lst.sort()
-
-        self.newlst = []
-        counter = 0
-        self.newlst.append(self.lst[0])
-        for i in self.lst:
-            if ignor_case is False:
-                if i.lower() != self.newlst[counter].lower():
-                    self.newlst.append(i)
-                    counter += 1
-            elif ignor_case is True:
-                if i != self.newlst[counter]:
-                    self.newlst.append(i)
-                    counter += 1
-
-        self.index = -1
-
+        
+        self.ignor_case = kwargs.get('ignore_case', False)
+        self.lst = (x for x in items)
+        self.newlst = list()
+    
     def __next__(self):
-        if self.index >= len(self.newlst) - 1:
+        try:
+            buffer = next(self.lst)
+            
+            if isinstance(buffer, str) and self.ignor_case is False:
+                if buffer.lower() not in self.newlst:
+                    self.newlst.append(buffer.lower())
+                    return buffer
+            elif isinstance(buffer, str) and self.ignor_case is True:
+                if buffer not in self.newlst:
+                    self.newlst.append(buffer)
+                    return buffer
+            else:
+                if buffer not in self.newlst:
+                    self.newlst.append(buffer)
+                    return buffer
+            return next(self)
+        
+        except Exception:
             raise StopIteration
-        self.index += 1
-        return self.newlst[self.index]
 
-    def __iter__(self):
-        return self
+def __iter__(self):
+    return self
